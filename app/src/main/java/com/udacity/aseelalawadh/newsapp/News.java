e2b46bde6877f264d095c0af8d6738f06f15ffe1
Aseel AlAwadh <aseel.alawadh@gmail.com>
2018-05-02 10:15:37 +0300

    changes base on reviwer

--- app/src/main/java/com/udacity/aseelalawadh/newsapp/News.java
+++ app/src/main/java/com/udacity/aseelalawadh/newsapp/News.java
@@ -10,7 +10,6 @@ public class News {
     public String date;
     public String newsDetails;
     public String newsUrl;
-    public String pillarName;
     public String authorName;
 
     public News(String section, String date, String newsDetails, String newsUrl, String pillarName, String authorName) {
@@ -18,48 +17,22 @@ public class News {
         this.date = date;
         this.newsDetails = newsDetails;
         this.newsUrl = newsUrl;
-        this.pillarName = pillarName;
         this.authorName = authorName;
     }
-
     public String getSection() {
         return section;
-    }
-
-    public void setSection(String section) {
-        this.section = section;
     }
 
     public String getDate() {
         return date;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-
     public String getNewsDetails() {
         return newsDetails;
     }
 
-    public void setNewsDetails(String newsDetails) {
-        this.newsDetails = newsDetails;
-    }
-
     public String getNewsUrl() {
         return newsUrl;
-    }
-
-    public void setNewsUrl(String newsUrl) {
-        this.newsUrl = newsUrl;
-    }
-
-    public String getPillarName() {
-        return pillarName;
-    }
-
-    public void setPillarName(String pillarName) {
-        this.pillarName = pillarName;
     }
 
     public String getAuthorName() {
